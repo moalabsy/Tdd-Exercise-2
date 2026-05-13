dde1c1fd39d67afc88094fa3db0d98f2b798f63a
Mo <alabsymohamad@gmail.com>
2026-05-13 11:19:03 +0200

    Red: test(TC_4) for [ 3, 5, 3, 5, 4, 2 ] should return 12

--- spec/electrons_game_spec.js
+++ spec/electrons_game_spec.js
@@ -17,4 +17,9 @@ describe('Electrons game', function() {
 	it('should return 2 for [ 6, 6, 4, 4, 1, 3 ] ', function() {
 		expect(game.play([ 6, 6, 4, 4, 1, 3 ] )).toEqual(2);
 	});
+
+	// TC_4
+	it('should return 12 for [ 3, 5, 3, 5, 4, 2 ] ', function() {
+		expect(game.play([ 3, 5, 3, 5, 4, 2 ] )).toEqual(12);
+	});
 });
